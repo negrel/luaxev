94cc53ee84b627d03fa5b41da51f4af80ee46869
Alexandre Negrel <alexandre@negrel.dev>
2025-04-24 13:42:43 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    let
+      outputsWithoutSystem = { };
+      outputsWithSystem = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          lib = pkgs.lib;
+        in
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [ zig ];
+            };
+          };
+        }
+      );
+    in
+    outputsWithSystem // outputsWithoutSystem;
+}
